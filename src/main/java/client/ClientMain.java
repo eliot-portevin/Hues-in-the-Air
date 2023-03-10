0642d3d0586e1fbe511bff7934cfe17c059bb92f
eliot-portevin <eliotportevin@gmail.com>
2023-03-10 12:53:49 +0100

    Changing local ip address to launch client

--- src/main/java/client/ClientMain.java
+++ src/main/java/client/ClientMain.java
@@ -2,7 +2,7 @@ package client;
 
 public class ClientMain {
     public static void main(String[] args) {
-        String[] serverInfo = {"10.192.5.243:9090"};
+        String[] serverInfo = {"25.20.244.173:9090"};
         try {
             Client.run(serverInfo);
         } catch (Exception e) {
