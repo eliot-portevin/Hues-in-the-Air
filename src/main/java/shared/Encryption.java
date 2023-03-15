4a6c94b971092dd4c06e1c2fb5264380f17ad1ed
jiri kaeser <jiri.kaeser@gmail.com>
2023-03-15 14:41:14 +0100

    Edited Encryption file to now work with simple algorithm

--- src/main/java/shared/Encryption.java
+++ src/main/java/shared/Encryption.java
@@ -8,13 +8,36 @@ public class Encryption {
      * Takes in a message and encrypts it.
      * */
     public static String encrypt(String message) {
+        int key = 5;
+        char[] chars = message.toCharArray();
+        int[] arr = new int[chars.length];
+        for(int i=0;i<chars.length;i++) {
+            arr[i] = (int) chars[i];
+            arr[i] += key;
+            chars[i] = (char) arr[i];
+        }
+        message = "";
+        for (char c : chars){
+            message += c;
+        }
         return message;
     }
-
     /**
      * Takes in a message and decrypts it.
      * */
     public static String decrypt(String message) {
+        int key = 5;
+        char[] chars = message.toCharArray();
+        int[] arr = new int[chars.length];
+        for(int i=0;i<chars.length;i++) {
+            arr[i] = (int) chars[i];
+            arr[i] -= key;
+            chars[i] = (char) arr[i];
+        }
+        message = "";
+        for (char c : chars){
+            message += c;
+        }
         return message;
     }
 }
