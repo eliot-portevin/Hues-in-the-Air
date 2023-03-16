e0f1d873f4f935ac865713c947dd59a18a7a8c60
Jennifer Florey <jennifer.florey@stud.unibas.ch>
2023-03-16 23:38:50 +0100

    Started writing Class GuiFirstAttempts which creates a JFrame

---
+++ src/main/java/ch/unibas/dmi/dbis/cs108/example/gui/GuiFirstAttempts.java
@@ -0,0 +1,14 @@
+package ch.unibas.dmi.dbis.cs108.example.gui;
+
+import javax.swing.*;
+
+public class GuiFirstAttempts {
+
+    public void createFrame() {
+        JFrame frame = new JFrame("Hues in the Air");
+        frame.setSize(700, 500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+}
