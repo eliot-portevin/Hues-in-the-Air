383087f0161958b892e21c0cac92ff5bbe7aa85a
eliot-portevin <eliotportevin@gmail.com>
2023-03-22 16:51:41 +0100

    Started parsing arguments given to main in order to either launch client or server

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,20 @@
-import server.*;
+import client.ClientMain;import server.*;
 //import client.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        if (args.length == 2) {
+            if (args[0].equals("client")) {
+                String[] split = args[1].split(":");
+                String hostAddress = split[0];
+                String port = split[1];
+
+                ClientMain.main(new String[]{hostAddress, port});
+            }
+            else if (args[0].equals("server")) {
+                ServerMain.main(new String[]{args[1]});
+            }
+        }
     }
 }
