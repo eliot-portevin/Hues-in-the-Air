c4a9d46629294a17734f8c2573fe353ec1616e1a
Eliot PORTEVIN <eliotportevin@gmail.com>
2023-03-31 17:26:41 +0200

    Importing custom fonts, changing text on alert button upon close.

--- src/main/java/gui/GuiJavaFX.java
+++ src/main/java/gui/GuiJavaFX.java
@@ -1,16 +1,15 @@
 package gui;
 
-import java.awt.*;import java.util.Optional;
+import java.awt.*;
+import java.util.Optional;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.DialogPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyCombination;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Screen;
@@ -19,12 +18,20 @@ import javafx.stage.StageStyle;
 
 public class GuiJavaFX extends Application {
 
+  // Screen resolution
   private double WIDTH = (int) Screen.getPrimary().getBounds().getWidth();
   private double HEIGHT = (int) Screen.getPrimary().getBounds().getHeight();
   private double scalingFactor = 1;
 
   Group root = new Group();
   private Stage stage;
+
+  // Fonts
+  private Font titleFont =
+      Font.loadFont(getClass().getResourceAsStream("/fonts/Raleway.ttf"), 100 * this.scalingFactor);
+  private Font textFont =
+      Font.loadFont(
+          getClass().getResourceAsStream("/fonts/Bebas_Neue_Regular.ttf"), 20 * this.scalingFactor);
 
   @Override
   public void start(Stage primaryStage) throws Exception {
@@ -38,7 +45,7 @@ public class GuiJavaFX extends Application {
 
     Text title = new Text();
     title.setText("Hues in the Air");
-    title.setFont(Font.font("Verdana", 50));
+    title.setFont(this.titleFont);
     title.setFill(Colours.WHITE.getHex());
     title.setX(this.WIDTH / 2 - title.getLayoutBounds().getWidth() / 2);
     title.setY(this.HEIGHT / 3);
@@ -46,21 +53,22 @@ public class GuiJavaFX extends Application {
     root.getChildren().add(title);
 
     // Set stage properties
-    this.stage.setOnCloseRequest(e -> {
-      e.consume();
-      this.handleEscape();
-    });
+    this.stage.setOnCloseRequest(
+        e -> {
+          e.consume();
+          this.handleEscape();
+        });
 
-    //this.stage.setFullScreen(true);
+    this.stage.setFullScreen(true);
     this.stage.setResizable(false);
     this.stage.getIcons().add(new Image("images/logo.jpg"));
     this.stage.show();
   }
 
-
   /**
-   * Creates a black scene depending on the screen resolution.
-   * The scene has a width of 16:9 and is scaled to fit the screen.
+   * Creates a black scene depending on the screen resolution. The scene has a width of 16:9 and is
+   * scaled to fit the screen.
+   *
    * @return scene
    */
   private Scene initScene() {
@@ -80,13 +88,18 @@ public class GuiJavaFX extends Application {
 
     this.scalingFactor *= 0.8;
 
-    return new Scene(this.root, this.WIDTH, this.HEIGHT, Colours.BLACK.getHex());
+    Scene scene = new Scene(this.root, this.WIDTH, this.HEIGHT, Colours.BLACK.getHex());
+    scene.getStylesheets().add("layout/default_style.css");
+    scene.getStylesheets().add("layout/title_font_style.css");
+    scene.getStylesheets().add("layout/text_font_style.css");
+
+    return scene;
   }
   /**
    * The user has pressed the escape key or clicked the close button. A dialog is shown to confirm
    * the user's intention to exit the game.
    */
-  public void handleEscape() {
+  private void handleEscape() {
     Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
     alert.setTitle("Hues in the Air");
 
@@ -98,10 +111,11 @@ public class GuiJavaFX extends Application {
 
     alert.setHeaderText("Are you sure you want to exit the game?");
     alert.initOwner(this.stage);
-    alert.initStyle(StageStyle.UNDECORATED);
+    alert.initStyle(StageStyle.UNIFIED);
+    alert.getButtonTypes().setAll(ButtonType.YES, ButtonType.CANCEL);
 
     Optional<ButtonType> result = alert.showAndWait();
-    if (result.orElse(null) == ButtonType.OK) {
+    if (result.orElse(null) == ButtonType.YES) {
       stage.close();
     }
   }
