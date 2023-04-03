244717d9e0cc4ce5941d6b635a6b8b1be9f9a05f
Nils Lerch <nils.lerch@stud.unibas.ch>
2023-04-03 18:56:18 +0200

    Implemented a simple Vector2D class and added Game and Cube classes

---
+++ src/main/java/server/Vector2D.java
@@ -0,0 +1,44 @@
+package server;
+
+import java.util.Vector;
+
+/**
+ * Represents a 2D Vector and various operations performed on 2D Vectors
+ */
+public class Vector2D {
+  private double xComponent;
+
+  private double yComponent;
+
+  public Vector2D(double xComponent, double yComponent) {
+    this.xComponent = xComponent;
+    this.yComponent = yComponent;
+  }
+
+  public double getX() {
+    return this.xComponent;
+  }
+
+  public double getY() {
+    return this.yComponent;
+  }
+
+  public void setX(double xComponent) {
+    this.xComponent = xComponent;
+  }
+
+  public void setY(double yComponent) {
+    this.yComponent = yComponent;
+  }
+
+  public void addInPlace(Vector2D vector2) {
+    this.xComponent += vector2.getX();
+    this.yComponent += vector2.getY();
+  }
+
+  public static Vector2D add(Vector2D vector1, Vector2D vector2) {
+    double newX = vector1.getX() + vector2.getX();
+    double newY = vector1.getY() + vector2.getY();
+    return new Vector2D(newX, newY);
+  }
+}
