cccfdd6405eb0472be4ef25ec9347a7d7cfec2ab
jiri kaeser <jiri.kaeser@gmail.com>
2023-04-06 13:41:13 +0200

    Added LevelData file which will contain all levels in probably string format. Added the most important methods to game.java, modified cube.java to work with game.java

--- src/main/java/server/Cube.java
+++ src/main/java/server/Cube.java
@@ -1,15 +1,44 @@
 package server;
 
+import gui.Colours;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.control.Button;
+
+import java.awt.*;
+
 public class Cube {
   private Vector2D position;
   private Vector2D velocity;
+  private Vector2D size;
   private double jumpHeight;
+  private boolean canJump;
+  private Pane gameRoot;
+  public Rectangle rectangle = new Rectangle();
 
-  public Cube(Vector2D position, Vector2D velocity) {
+  public Cube(Pane gameRoot, Vector2D position, Vector2D velocity, Vector2D size) {
     this.position = position;
     this.velocity = velocity;
+    this.size = size;
+    this.gameRoot = gameRoot;
+    spawnCube();
   }
 
+  public void spawnCube() {
+    rectangle.setX(position.getX());
+    rectangle.setY(position.getY());
+    rectangle.setWidth(size.getX());
+    rectangle.setHeight(size.getX());
+    gameRoot.getChildren().add(rectangle);
+  }
   public void jump() {
 
   }
--- src/main/java/server/Game.java
+++ src/main/java/server/Game.java
@@ -1,61 +1,102 @@
 package server;
+import java.util.ArrayList;
 
 import gui.Colours;
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.control.Button;
 
 import java.awt.*;
+import java.util.HashMap;
 
 public class Game extends Application {
+  private HashMap<KeyCode, Boolean> keys = new HashMap<KeyCode, Boolean>();
+  private ArrayList<Node> platforms = new ArrayList<Node>(); // Used to store platforms
+  private ArrayList<Node> stars = new ArrayList<Node>(); // Used to store collectable stars
+  private Pane appRoot = new Pane();
+    private Pane gameRoot = new Pane();
+    private Pane uiRoot = new Pane();
+    private Cube player;
+    private int levelWidth;
+
   public static void main(String[] args) {
     Application.launch(args);
   }
 
+  public void update(){
+    if (isPressed(KeyCode.UP)) {
+      player.rectangle.setTranslateY(player.rectangle.getTranslateY()- 5);
+    }
+    if (isPressed(KeyCode.DOWN)) {
+      player.rectangle.setTranslateY(player.rectangle.getTranslateY() + 5);
+    }
+    if (isPressed(KeyCode.LEFT)) {
+      player.rectangle.setTranslateX(player.rectangle.getTranslateX() - 5);
+    }
+    if (isPressed(KeyCode.RIGHT)) {
+      player.rectangle.setTranslateX(player.rectangle.getTranslateX() + 5);
+    }
+  }
+
+
+  private boolean isPressed(KeyCode keyCode) {
+    return keys.getOrDefault(keyCode, false);
+  }
+
+  public void initializeContent() {
+    Rectangle bg = new Rectangle(800, 600);
+    levelWidth = LevelData.Level1[0].length() * 50;
+    // Create a rectangle with 100px width and 50px height at position (0, 0)
+    Cube cube = new Cube(gameRoot, new Vector2D(350,175),  new Vector2D(0,0), new Vector2D(100,100));
+    // Set the fill color of the rectangle
+    cube.rectangle.setFill(Color.RED);
+    // Add the rectangle to the root
+  }
+
   @Override
   public void start(Stage primaryStage) throws Exception {
-    Group root = new Group();
-    Scene scene = new Scene(root,800,450, Color.BLUE);
-    Stage stage = new Stage();
+    initializeContent();
+    Scene scene = new Scene(gameRoot);
+    scene.setOnKeyPressed(event -> keys.put(event.getCode(), true));
+    scene.setOnKeyReleased(event -> keys.put(event.getCode(), false));
 
-    Rectangle rectangle = new Rectangle();
-    rectangle.setX(350);
-    rectangle.setY(175);
-    rectangle.setWidth(100);
-    rectangle.setHeight(100);
+    primaryStage.setTitle("Game");
+    primaryStage.setScene(scene);
+    primaryStage.show();
+
+    AnimationTimer timer = new AnimationTimer() {
+      @Override
+      public void handle(long now) {
+        update();
+      }
+    };
+    timer.start();
 
 
+    /**
+    Cube cube = new Cube(gameRoot, new Vector2D(350,175),  new Vector2D(0,0), new Vector2D(100,100));
 
-    root.getChildren().add(rectangle);
     Button btup = new Button("UP");
     btup.setLayoutX(350);
     btup.setLayoutY(325);
 
-    root.getChildren().add(btup);
+    gameRoot.getChildren().add(btup);
 
-    btup.setOnAction(e -> rectangle.setY(rectangle.getY()-10));
-
-    stage.setScene(scene);
-    stage.show();
+    btup.setOnAction(e -> cube.rectangle.setY(cube.rectangle.getY()-10));
+      */
 
 
   }
-
-  /*EventHandler<ActionEvent> btupActionEvent = new EventHandler<ActionEvent>() {
-    @Override
-    public void handle(ActionEvent event) {
-      System.out.println("Pressed up");
-
-      rectangle.setX()
-
-    }
-  };*/
 
 }
