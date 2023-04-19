bf8e83376e5fb65faf0400993eece7476339c48d
Jennifer Florey <jennifer.florey@stud.unibas.ch>
2023-04-19 13:41:01 +0200

    Started creating Junit tests

---
+++ src/test/java/clientTests/ServerOutTest.java
@@ -0,0 +1,25 @@
+package clientTests;
+
+import client.Client;
+import client.ServerOut;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.Socket;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ServerOutTest {
+    @Test
+    void validateMessageNullShouldBeNull() throws IOException {
+
+        Socket serverSocket = new Socket();
+        Client client = new Client();
+        ServerOut socket = new ServerOut(serverSocket, client);
+        String message = null;
+        assertNull(socket.validateMessage(message));
+
+    }
+
+
+}
