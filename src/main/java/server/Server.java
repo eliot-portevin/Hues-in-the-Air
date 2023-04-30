60c1ccfe38c40347281c9ecb2b022465090f8e5e
Jennifer Florey <jennifer.florey@stud.unibas.ch>
2023-04-30 19:13:04 +0200

    Updated checkstyle in Server.java and ServerCube.java

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -4,34 +4,52 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketException;
-import java.util.*;
+import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 /**
- * The server class. This class is responsible for managing the server. It contains a list of all
- * clients, a list of all lobbies and a list of all games. It also contains a ServerSocket which is
+ * The server class. This class is responsible for managing the server.
+ * It contains a list of all clients, a list of all lobbies and a list
+ * of all games. It also contains a ServerSocket which is
  * used to listen for client connections.
  */
 public class Server implements Runnable {
 
-  private final int PORT;
+  /** the port of the server. */
+  private final int port;
 
+  /** An ArrayList for all clientHandlers. */
   private final ArrayList<ClientHandler> clientHandlers = new ArrayList<>();
+  /** An ArrayList for the Threads of the clients. */
   private final ArrayList<Thread> clientThreads = new ArrayList<>();
+  /** A HashMap for all lobbies with their names. */
   private final HashMap<String, Lobby> lobbies = new HashMap<>();
-  private Map<ServerGame, Map.Entry<Integer, Boolean>> games = new LinkedHashMap<>();
+  /** A Map for the ServerGame used for the game state. */
+  private Map<ServerGame, Map.Entry<Integer, Boolean>> games
+          = new LinkedHashMap<>();
 
+  /** The listener of the ServerSocket. */
   private ServerSocket listener;
-
+  /** Used to try to connect the client to the server. */
   private boolean shuttingDown = false;
+  /** Used to know if clients are connected to the server. */
   private boolean noClientConnected = true;
 
+
+  /** The instance of the server. */
   private static Server instance;
   /** The logger for the server. */
-  protected final Logger LOGGER;
+  private final Logger logger;
 
   /** The maximum amount of characters a username can have. */
   public static final int MAX_NAME_LENGTH = 24;
@@ -39,22 +57,24 @@ public class Server implements Runnable {
   /**
    * Creates a new server.
    *
-   * @param PORT The port which the server will listen on
+   * @param serverPort The port which the server will listen on
    */
-  public Server(int PORT) {
-    this.PORT = PORT;
-    this.LOGGER = LogManager.getLogger(Server.class);
+  public Server(final int serverPort) {
+    this.port = serverPort;
+    this.logger = LogManager.getLogger(Server.class);
     instance = this;
   }
 
   /**
-   * Waits for client connections. When a client wants to connect to the server, the addClient
+   * Waits for client connections.
+   * When a client wants to connect to the server, the addClient
    * method is called.
    */
   public void run() {
     try {
-      this.listener = new ServerSocket(this.PORT);
-      Thread pingSender = new Thread(new ServerPingSender(this.clientHandlers, this));
+      this.listener = new ServerSocket(this.port);
+      Thread pingSender
+              = new Thread(new ServerPingSender(this.clientHandlers, this));
 
       while (true) {
         if (!shuttingDown) {
@@ -68,7 +88,7 @@ public class Server implements Runnable {
             // LOGGER.info("Client was added");
           } catch (SocketException e) {
             if (this.shuttingDown) {
-              LOGGER.error("Unable to connect with message: " + e.getMessage());
+              logger.error("Unable to connect with message: " + e.getMessage());
               break;
             }
           }
@@ -77,18 +97,19 @@ public class Server implements Runnable {
         }
       }
     } catch (IOException e) {
-      LOGGER.error("Server exception: " + e.getMessage());
+      logger.error("Server exception: " + e.getMessage());
       e.printStackTrace();
     }
   }
 
   /**
-   * Adds a client to the list of clients and starts a dedicated thread for the client.
+   * Adds a client to the list of clients
+   * and starts a dedicated thread for the client.
    *
    * @param clientSocket The socket of the client
    * @throws IOException If the client socket is closed
    */
-  private void addClient(Socket clientSocket) throws IOException {
+  private void addClient(final Socket clientSocket) throws IOException {
     ClientHandler clientHandler = new ClientHandler(clientSocket, this);
     this.clientHandlers.add(clientHandler);
 
@@ -96,19 +117,19 @@ public class Server implements Runnable {
     this.clientThreads.add(clientThread);
     clientThread.start();
 
-    LOGGER.info("[Server] Connected to Client!");
+    logger.info("[Server] Connected to Client!");
     this.updateLobbyList();
     this.updateClientList();
   }
 
   /**
    * Called from {@link ClientHandler} when a client disconnects ({@link
-   * client.ClientProtocol#EXIT}). Removes the client from the list of clients, from its lobby and
-   * interrupts the client's dedicated thread.
+   * client.ClientProtocol#EXIT}). Removes the client from the list of clients,
+   * from its lobby and interrupts the client's dedicated thread.
    *
    * @param client The client that disconnected
    */
-  protected void removeClient(ClientHandler client) {
+  protected void removeClient(final ClientHandler client) {
     client.setRunning(false);
     Optional<Lobby> lobby = Optional.ofNullable(client.getLobby());
     lobby.ifPresent(value -> value.removeClient(client));
@@ -116,7 +137,7 @@ public class Server implements Runnable {
     this.clientThreads.remove(this.clientHandlers.indexOf(client));
     this.clientHandlers.remove(client);
 
-    LOGGER.info("Client " + client.getUsername() + " disconnected");
+    logger.info("Client " + client.getUsername() + " disconnected");
     this.updateLobbyList();
     this.updateClientList();
   }
@@ -126,14 +147,15 @@ public class Server implements Runnable {
    *
    * @param lobby The lobby that should be removed
    */
-  protected void removeLobby(Lobby lobby) {
+  protected void removeLobby(final Lobby lobby) {
     this.lobbies.remove(lobby.getName());
     this.updateLobbyList();
-    LOGGER.info("The lobby " + lobby.getName() + " was removed because it was empty.");
+    logger.info("The lobby " + lobby.getName()
+            + " was removed because it was empty.");
   }
 
   /**
-   * Called from {@link ClientHandler} when a client disconnects
+   * Called from {@link ClientHandler} when a client disconnects.
    *
    * @throws IOException If the socket fails to close
    */
@@ -148,7 +170,7 @@ public class Server implements Runnable {
       removeClient(client);
     }
 
-    LOGGER.info("Server shutting down.");
+    logger.info("Server shutting down.");
     System.exit(0);
   }
 
@@ -162,7 +184,8 @@ public class Server implements Runnable {
   }
 
   /**
-   * @return The games that have been played, or are still playing, and their status
+   * @return The games that have been played,
+   * or are still playing, and their status.
    */
   protected Map<ServerGame, AbstractMap.Entry<Integer, Boolean>> getGames() {
     return this.games;
@@ -173,22 +196,24 @@ public class Server implements Runnable {
     List<Map.Entry<ServerGame, Map.Entry<Integer, Boolean>>> list =
         new LinkedList<>(this.games.entrySet());
 
-    list.sort(((o1, o2) -> o2.getValue().getKey().compareTo(o1.getValue().getKey())));
+    list.sort(((o1, o2)
+            -> o2.getValue().getKey().compareTo(o1.getValue().getKey())));
 
     this.games =
         list.stream()
             .collect(
                 Collectors.toMap(
-                    Map.Entry::getKey, Map.Entry::getValue, (a, b) -> b, LinkedHashMap::new));
+                    Map.Entry::getKey, Map.Entry::getValue,
+                        (a, b) -> b, LinkedHashMap::new));
   }
 
   /**
-   * Returns the name of the client for given username
+   * Returns the name of the client for given username.
    *
    * @param username of the client
    * @return The name of the client
    */
-  protected ClientHandler getClientHandler(String username) {
+  protected ClientHandler getClientHandler(final String username) {
     for (ClientHandler client : this.clientHandlers) {
       if (client.getUsername() != null) {
         if (client.getUsername().equals(username)) {
@@ -200,36 +225,39 @@ public class Server implements Runnable {
   }
 
   /**
-   * Called from {@link ClientHandler} when a client wants to create a lobby Creates a new lobby and
-   * adds the client to it.
+   * Called from {@link ClientHandler} when a client wants to create a lobby.
+   * Creates a new lobby and adds the client to it.
    *
    * @param lobbyName The name of the lobby
    * @param password The password of the lobby
    * @param client The client that wants to create the lobby
    */
-  protected void createLobby(String lobbyName, String password, ClientHandler client) {
+  protected void createLobby(final String lobbyName, final String password,
+                             final ClientHandler client) {
     for (String lobby : this.lobbies.keySet()) {
       if (lobby.equals(lobbyName)) {
         // Lobby already exists
-        LOGGER.warn("Lobby does already exist and cannot be created.");
+        logger.warn("Lobby does already exist and cannot be created.");
         return;
       }
     }
 
     this.lobbies.put(lobbyName, new Lobby(lobbyName, password));
-    LOGGER.info(client.getUsername() + " created lobby " + lobbyName + ".");
+    logger.info(client.getUsername() + " created lobby " + lobbyName + ".");
     this.lobbies.get(lobbyName).addClient(client, password);
   }
 
   /**
-   * Called from {@link ClientHandler} when a client wants to join a lobby Adds the client to the
+   * Called from {@link ClientHandler} when a client wants to join a lobby
+   * Adds the client to the
    * lobby.
    *
    * @param lobbyName The name of the lobby
    * @param password The password of the lobby
    * @param client The client that wants to join the lobby
    */
-  protected void joinLobby(String lobbyName, String password, ClientHandler client) {
+  protected void joinLobby(final String lobbyName, final String password,
+                           final ClientHandler client) {
     for (String lobby : this.lobbies.keySet()) {
       if (lobby.equals(lobbyName)) {
         this.lobbies.get(lobbyName).addClient(client, password);
@@ -240,8 +268,8 @@ public class Server implements Runnable {
   }
 
   /**
-   * Sends a message to all clients in the server containing the list of all lobbies and their
-   * clients.
+   * Sends a message to all clients in the server
+   * containing the list of all lobbies and their clients.
    */
   protected void updateLobbyList() {
     synchronized (this.lobbies) {
@@ -256,7 +284,10 @@ public class Server implements Runnable {
     }
   }
 
-  /** Sends a message to all clients in the server containing the list of all clients. */
+  /**
+   * Sends a message to all clients in the server
+   * containing the list of all clients.
+   */
   protected void updateClientList() {
     synchronized (this.clientHandlers) {
       for (ClientHandler client : this.clientHandlers) {
@@ -266,8 +297,8 @@ public class Server implements Runnable {
   }
 
   /**
-   * Sends a message to all clients in the server containing the list of all games and their status
-   * (running or finished).
+   * Sends a message to all clients in the server containing
+   * the list of all games and their status (running or finished).
    */
   private void updateGameList() {
     this.sortGames();
@@ -280,7 +311,8 @@ public class Server implements Runnable {
   }
 
   /**
-   * Produces an array of all lobbies and their clients. Called from {@link ClientHandler} each time
+   * Produces an array of all lobbies and their clients.
+   * Called from {@link ClientHandler} each time
    * the client list is updated. This list is then sent to the client.
    *
    * @return An array of all lobbies and their clients
@@ -300,7 +332,8 @@ public class Server implements Runnable {
   }
 
   /**
-   * Called from {@link ClientHandler} for the logger and from {@link Lobby} to update the lists
+   * Called from {@link ClientHandler} for the logger and from {@link Lobby}
+   * to update the lists.
    *
    * @return the instance of the server
    */
@@ -309,26 +342,31 @@ public class Server implements Runnable {
   }
 
   /**
-   * Called from {@link Lobby} when a game has been started. Adds the game to the list of games and
-   * sends a message to all clients in the server containing the list of all games. The info stored
-   * in the map is: game instance, levels completed, whether the game is running or not.
+   * Called from {@link Lobby} when a game has been started.
+   * Adds the game to the list of games and sends a message to all clients
+   * in the server containing the list of all games. The info stored
+   * in the map is: game instance, levels completed,
+   * whether the game is running or not.
    *
    * @param game The game instance that has been started
    */
-  protected void addGame(ServerGame game) {
-    this.games.put(game, new AbstractMap.SimpleEntry<>(game.getLevelsCompleted(), true));
+  protected void addGame(final ServerGame game) {
+    this.games.put(game,
+            new AbstractMap.SimpleEntry<>(game.getLevelsCompleted(), true));
     this.updateGameList();
   }
 
   /**
-   * Called from {@link ServerGame} when a game has ended. Sets the game to finished and sends a
-   * message to all clients in the server containing the list of all games. The info stored in the
-   * map is: game instance, levels completed, whether the game is running or not.
+   * Called from {@link ServerGame} when a game has ended.
+   * Sets the game to finished and sends a message to all clients in the server
+   * containing the list of all games. The info stored in the map is:
+   * game instance, levels completed, whether the game is running or not.
    *
    * @param game The game instance that has ended
    */
-  protected void endGame(ServerGame game) {
-    this.games.put(game, new AbstractMap.SimpleEntry<>(game.getLevelsCompleted(), false));
+  protected void endGame(final ServerGame game) {
+    this.games.put(game,
+            new AbstractMap.SimpleEntry<>(game.getLevelsCompleted(), false));
 
     for (ClientHandler client : game.getPlayers()) {
       client.gameEnded();
@@ -338,15 +376,17 @@ public class Server implements Runnable {
   }
 
   /**
-   * Called from {@link ServerGame#nextLevel()} when a level has been completed. Updates the number
-   * of levels completed and sends a message to all clients in the server containing the list of all
-   * games. The info stored in the map is: game instance, levels completed, whether the game is
-   * running or not.
+   * Called from {@link ServerGame#nextLevel()} when a level has been completed.
+   * Updates the number of levels completed and sends a message to all clients
+   * in the server containing the list of all games.
+   * The info stored in the map is: game instance, levels completed,
+   * whether the game is running or not.
    *
    * @param game The game instance that has completed a level
    */
-  protected void updateGameLevelsCompleted(ServerGame game) {
-    this.games.put(game, new AbstractMap.SimpleEntry<>(game.getLevelsCompleted(), true));
+  protected void updateGameLevelsCompleted(final ServerGame game) {
+    this.games.put(game,
+            new AbstractMap.SimpleEntry<>(game.getLevelsCompleted(), true));
     this.updateGameList();
   }
 }
--- src/main/java/server/ServerCube.java
+++ src/main/java/server/ServerCube.java
@@ -8,7 +8,8 @@ import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 
 /**
- * The server version of a cube. Used to handle the logic of the cube on the server side.
+ * The server version of a cube.
+ * Used to handle the logic of the cube on the server side.
  */
 public class ServerCube extends Cube {
   /**
@@ -17,43 +18,47 @@ public class ServerCube extends Cube {
    * @param gameRoot The pane which the cube will be moving on
    * @param spawnPosition The position which the cube will spawn at
    */
-  public ServerCube(Pane gameRoot, Vector2D spawnPosition) {
+  public ServerCube(final Pane gameRoot, final Vector2D spawnPosition) {
     super(gameRoot, spawnPosition);
   }
 
   /**
-   * Checks whether the player is currently colliding with the coin (end of the level). If that is
-   * the case, the next level is loaded. Called from {@link ServerCube#move(Block[], double)}.
+   * Checks whether the player is currently colliding with the coin.
+   * (end of the level) If that is the case, the next level is loaded.
+   * Called from {@link ServerCube#move(Block[], double)}.
    *
    * @param block The block which might be a coin
    */
   @Override
-  public void checkCoinCollision(Block block) {
+  public void checkCoinCollision(final Block block) {
     if (block.getColour().equals(Colours.TRANSPARENT.getHex())) {
       ServerGame.getInstance().nextLevel();
     }
   }
 
   /**
-   * The cube has entered in contact with a white cube. Its position, velocity and acceleration are
-   * reset. The game instance is also informed of this event so that a life can be deducted. See
-   * {@link ServerGame#die()}.
+   * The cube has entered in contact with a white cube.
+   * Its position, velocity and acceleration are reset.
+   * The game instance is also informed of this event
+   * so that a life can be deducted. See {@link ServerGame#die()}.
    */
   @Override
   public void die() {
-    if (ServerGame.getInstance() != null) ServerGame.getInstance().die();
+    if (ServerGame.getInstance() != null)  {
+      ServerGame.getInstance().die();
+    }
   }
 
   /**
-   * Calls the super method to jump. If a jump has successfully been performed, all clients in the
-   * game are informed of the jump (the coordinates of the rotation point). See {@link
-   * ServerGame#jumpUpdate()}.
+   * Calls the super method to jump. If a jump has successfully been performed,
+   * all clients in the game are informed of the jump (the coordinates
+   * of the rotation point). See {@link ServerGame#jumpUpdate()}.
    *
-   * @param Colour the colour of the player who requested a jump
+   * @param colour the colour of the player who requested a jump
    */
   @Override
-  public void jump(Color Colour) {
-    super.jump(Colour);
+  public void jump(final Color colour) {
+    super.jump(colour);
 
     if (jumping) {
       ServerGame.getInstance().jumpUpdate();
