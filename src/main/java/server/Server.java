07fa1c198b66580bca8677d876999fd83f6fabd9
eliot-portevin <eliotportevin@gmail.com>
2023-05-17 14:48:12 +0200

    All highscores are saved and created gameplay folder

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -425,14 +425,6 @@ public class Server implements Runnable {
         this.highscores.add(line.replace("true", "false"));
       }
     }
-
-    String[] array = this.highscores.toArray(new String[0]);
-    util.SortHighscores.sort(array);
-
-    this.highscores.clear();
-    if (array.length > 0) {
-      this.highscores.addAll(Arrays.asList(array).subList(0, Math.min(array.length, 10)));
-    }
   }
 
   /**
